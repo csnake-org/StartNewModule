48b2cb1579b7104f0cc2704bd8e0b0747ef6980a
ymartelli <ymartelli@941262cd-e152-0410-93b7-8f0684ea2129>
2010-04-20 10:23:44 +0000

    Updated dialog.
    git-svn-id: https://svncistib.upf.edu/repos/toolkit/trunk/tools/StartNewModule@12108 941262cd-e152-0410-93b7-8f0684ea2129

--- src/helpDialog.py
+++ src/helpDialog.py
@@ -3,9 +3,21 @@ import wx
 class HelpDialog(wx.Dialog):
     def __init__(self, *args, **kwds):
         # begin wxGlade: HelpDialog.__init__
-        kwds["style"] = wx.DEFAULT_DIALOG_STYLE
+        kwds["style"] = wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.THICK_FRAME
         wx.Dialog.__init__(self, *args, **kwds)
-        self.textBasicHelp = wx.TextCtrl(self, -1, "First This, then that...", style=wx.TE_MULTILINE|wx.TE_READONLY)
+        
+        text = ""
+        text += "1. Put the name you want for your module in field Name\n"
+        text += "2. Set the root path to your module:\n"
+        text += "    * for type \"library\" gimiasSRC/src/cilabModules\n"
+        text += "    * for type \"Gimias Plugin\" gimiasSRC/src/Apps/Plugins\n"
+        text += "    * for type \"Gimias Plugin widget\" gimiasSRC/src/Apps/Plugins/YourPlugin\n"
+        text += "3. Select Type \"library\" or \"Gimias Plugin\" or \"Gimias Plugin widget\"\n"
+        text += "4. (for type \"library\" and \"Gimias Plugin\" only) Set Toolkit csn file to your gimiasSRC/src/csnCISTIBToolkit.py\n"
+        text += "5. (for type \"Gimias Plugin\" only) Set your Gimias csn file to your gimiasSRC/src/Apps/csnGIMIASOS.py\n"
+        text += "6. Press on StartNewModule\n"
+
+        self.textBasicHelp = wx.TextCtrl(self, -1, text, style=wx.TE_MULTILINE|wx.TE_READONLY)
 
         self.__set_properties()
         self.__do_layout()
@@ -14,6 +26,7 @@ class HelpDialog(wx.Dialog):
     def __set_properties(self):
         # begin wxGlade: HelpDialog.__set_properties
         self.SetTitle("Basic Help")
+        self.SetSize((400, 200))
         # end wxGlade
 
     def __do_layout(self):
@@ -21,6 +34,5 @@ class HelpDialog(wx.Dialog):
         sizer_1 = wx.BoxSizer(wx.HORIZONTAL)
         sizer_1.Add(self.textBasicHelp, 1, wx.ALL|wx.EXPAND, 5)
         self.SetSizer(sizer_1)
-        sizer_1.Fit(self)
         self.Layout()
         # end wxGlade
