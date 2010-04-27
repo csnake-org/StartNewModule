f5392c8a36636b79dc55dc6403bb1e28ad0a1509
ymartelli <ymartelli@941262cd-e152-0410-93b7-8f0684ea2129>
2010-04-27 08:04:55 +0000

    Added installer configuration and modified setup.
    git-svn-id: https://svncistib.upf.edu/repos/toolkit/trunk/tools/StartNewModule@12211 941262cd-e152-0410-93b7-8f0684ea2129

--- src/setup.py
+++ src/setup.py
@@ -9,31 +9,23 @@ from about import About
 class exeSetup():
     ''' Helper class to generate windows executable.'''
     def __init__(self):
-        pathToResources = "../resources/"
+        pathToResources = "../resources"
         pathToSrc = "../src/"
         # Resource files to add to bin folder.
         self.resources = []
-        f1 = os.path.join(pathToResources, "about.txt")
-        f2 = "resources", [f1]
-        self.resources.append(f2)
-        f1 = os.path.join(pathToResources, "logging.conf")
-        f2 = "resources", [f1]
-        self.resources.append(f2)
-        # Template code
-        folderToParse = ["TemplateLibrary", "TemplatePlugin"]
-        for folder in folderToParse:
-            for root, dirs, files in os.walk(pathToResources + folder):
-                if not root.find( ".svn" ) == -1: # skip svn hidden directories
-                    continue
-                for name in files:
-                    f1 = os.path.join(root, name)
-                    f2 = "resources/" + root, [f1]
-                    self.resources.append(f2)
+        for root, dirs, files in os.walk(pathToResources):
+            if not root.find( ".svn" ) == -1: # skip svn hidden directories
+                continue
+            for name in files:
+                f1 = os.path.join(root, name)
+                f2 = "resources/" + root, [f1]
+                self.resources.append(f2)
         # script file
         self.script = pathToSrc + "StartNewModule.py"
+        self.icon_resource = pathToResources + "/startnewmodule.ico"
         # options
         about = About()
-        about.read( pathToResources + "about.txt")
+        about.read( pathToResources + "/about.txt")
         self.name = about.getName()
         self.version = about.getVersion()
         self.description = about.getDescription()
@@ -49,6 +41,7 @@ class exeSetup():
             windows=[ 
                 {
                     "script": self.script,
+                    "icon_resources": [(1, self.icon_resource)]
                 }
             ],
             data_files = self.resources )
