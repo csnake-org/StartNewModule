a98a9405619d9369718ceb14db1cf2afcc45e061
ymartelli <ymartelli@941262cd-e152-0410-93b7-8f0684ea2129>
2010-04-27 10:20:22 +0000

    Added documentation.
    git-svn-id: https://svncistib.upf.edu/repos/toolkit/trunk/tools/StartNewModule@12220 941262cd-e152-0410-93b7-8f0684ea2129

--- tests/aboutTests.py
+++ tests/aboutTests.py
@@ -1,16 +1,17 @@
-# Tests for the About class
+""" Definition of the AboutTests class. """
 import unittest
 from about import About
 import os
 
 class AboutTests(unittest.TestCase):
-
+    """ Tests of the About class. """
     def setUp(self):
         """ Run before test. """
 
     def tearDown(self):
         """ Run after test. """
 
+    ## \test Test read and write of an about file.
     def testReadWrite(self):
         """ AboutTests: testReadWrite. """
         filename = "about_test.txt"
