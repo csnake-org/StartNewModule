9a28b952116a82ea220da7632ce354e9db030589
ymartelli <ymartelli@941262cd-e152-0410-93b7-8f0684ea2129>
2010-05-10 03:29:26 +0000

    Simplified csn file.
    git-svn-id: https://svncistib.upf.edu/repos/toolkit/trunk/tools/StartNewModule@12449 941262cd-e152-0410-93b7-8f0684ea2129

--- resources/TemplateLibrary/csnTemplateLibraryApps.py
+++ resources/TemplateLibrary/csnTemplateLibraryApps.py
@@ -1,12 +1,9 @@
 # Used to configure TemplateLibraryApps
-
-import csnBuild
 import csnCilab
 import csnUtility
-import csnTemplateLibrary
 from csnCISTIBToolkit import *
 
 templateLibraryApps = csnCilab.CilabModuleProject("TemplateLibraryApps", "library")
 templateLibraryApps.AddSources( [csnUtility.GetDummyCppFilename()] )
-templateLibraryApps.AddProjects( [csnTemplateLibrary.templateLibrary] )
+templateLibraryApps.AddProjects( [templateLibrary] )
 templateLibraryApps.AddApplications([ "tlAppFirst" ], _holderName="TemplateLibraryApplications")
