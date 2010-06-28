d7ea245dd04438d27c0a99820210d653d4756ccb
ymartelli <ymartelli@941262cd-e152-0410-93b7-8f0684ea2129>
2010-06-28 09:52:25 +0000

    Updated layout to fit in window.
    git-svn-id: https://svncistib.upf.edu/repos/toolkit/trunk/tools/StartNewModule@13114 941262cd-e152-0410-93b7-8f0684ea2129

--- src/helpDialog.py
+++ src/helpDialog.py
@@ -9,17 +9,23 @@ class HelpDialog(wx.Frame):
         text = ""
         text += "1. Put the name you want for your module in field Name\n"
         text += "    * for type \"library\" put something like MyNewNameLib\n"
-        text += "    * for type \"Gimias Plugin\" put something like MyNewNamePlugin \n"
-        text += "    * for type \"Gimias Plugin widget\" put only MyNewFunctionality it will add automatically PanelWidget and Processor\n"
+        text += "    * for type \"Gimias Plugin\" put something like MyNewNamePlugin\n"
+        text += "    * for type \"Gimias Plugin widget\" put only MyNewFunctionality it will add\n"
+        text += "       automatically PanelWidget and Processor\n"
         text += "    * for type \"ThirdParty\" put something likt MyNewThirdParty\n"
         text += "2. Set the root path to your module:\n"
-        text += "    * for type \"library\" MySrcFolder/src/cilabModules or MySrcFolder/src/modules \n"
-        text += "    * for type \"Gimias Plugin\" MySrcFolder/src/Apps/Plugins or MySrcFolder/src/plugins\n"
-        text += "    * for type \"Gimias Plugin widget\" MySrcFolder/src/Apps/Plugins/MyNewNamePlugin or MySrcFolder/src/plugins/MyNewNamePlugin\n"
+        text += "    * for type \"library\" MySrcFolder/src/cilabModules or MySrcFolder/src/modules\n"
+        text += "    * for type \"Gimias Plugin\" MySrcFolder/src/Apps/Plugins or\n"
+        text += "       MySrcFolder/src/plugins\n"
+        text += "    * for type \"Gimias Plugin widget\" MySrcFolder/src/Apps/Plugins/MyNewNamePlugin\n"
+        text += "       or MySrcFolder/src/plugins/MyNewNamePlugin\n"
         text += "    * for type \"ThirdParty\" MySrcFolder/thirdParty or MySrcFolder/src/thirdparties\n"
-        text += "3. Select Type \"library\" or \"Gimias Plugin\" or \"Gimias Plugin widget\" or \"ThirdParty\"\n"
-        text += "4. (for type \"library\", \"Gimias Plugin\" and \"ThirdParty\"  only) Set Toolkit csn file to your MySrcFolder/src/csnCISTIBToolkit.py or csnMyToolkit.py \n"
-        text += "5. (for type \"Gimias Plugin\" only) Set your Gimias csn file to your MySrcFolder/src/Apps/csnGIMIAS.py or MySrcFolder/src/csnGIMIAS.py \n"
+        text += "3. Select Type \"library\" or \"Gimias Plugin\" or \"Gimias Plugin widget\"\n"
+        text += "    or \"ThirdParty\"\n"
+        text += "4. (for type \"library\", \"Gimias Plugin\" and \"ThirdParty\"  only) Set Toolkit csn file\n"
+        text += "    to your MySrcFolder/src/csnCISTIBToolkit.py or csnMyToolkit.py\n"
+        text += "5. (for type \"Gimias Plugin\" only) Set your Gimias csn file to your\n"
+        text += "    MySrcFolder/src/Apps/csnGIMIAS.py or MySrcFolder/src/csnGIMIAS.py\n"
         text += "6. Press on StartNewModule\n"
 
         self.textBasicHelp = wx.TextCtrl(self, -1, text, style=wx.TE_MULTILINE|wx.TE_READONLY)
@@ -31,7 +37,7 @@ class HelpDialog(wx.Frame):
     def __set_properties(self):
         # begin wxGlade: HelpDialog.__set_properties
         self.SetTitle("Basic Help")
-        self.SetSize((400, 300))
+        self.SetSize((600, 350))
         # end wxGlade
 
     def __do_layout(self):
