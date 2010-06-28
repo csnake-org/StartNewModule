6bde4859b4c85c246e553c2be913437dc40ee30b
ymartelli <ymartelli@941262cd-e152-0410-93b7-8f0684ea2129>
2010-06-28 05:49:53 +0000

    Adapted to new csnake (2.3.2) syntax. Removed support for old version.
    git-svn-id: https://svncistib.upf.edu/repos/toolkit/trunk/tools/StartNewModule@13105 941262cd-e152-0410-93b7-8f0684ea2129

--- resources/TemplateThirdParty/csnTemplateThirdParty.py
+++ resources/TemplateThirdParty/csnTemplateThirdParty.py
@@ -2,12 +2,8 @@
 import csnBuild
 
 templateThirdParty = csnBuild.Project("TemplateThirdParty", "third party")
-if csnBuild.version >= 2.22:
-    templateThirdParty.pathsManager.useFilePath = "%s/TemplateThirdParty/UseTemplateThirdParty.cmake" % templateThirdParty.context.GetThirdPartyBuildFolder()
-    templateThirdParty.pathsManager.configFilePath = "%s/TemplateThirdParty/TemplateThirdPartyConfig.cmake" % templateThirdParty.context.GetThirdPartyBuildFolder()
-else:
-    templateThirdParty.pathsManager.useFilePath = "%s/TemplateThirdParty/UseTemplateThirdParty.cmake" % templateThirdParty.context.thirdPartyBuildFolder
-    templateThirdParty.pathsManager.configFilePath = "%s/TemplateThirdParty/TemplateThirdPartyConfig.cmake" % templateThirdParty.context.thirdPartyBuildFolder
+templateThirdParty.pathsManager.useFilePath = "%s/TemplateThirdParty/UseTemplateThirdParty.cmake" % templateThirdParty.GetBuildFolder()
+templateThirdParty.pathsManager.configFilePath = "%s/TemplateThirdParty/TemplateThirdPartyConfig.cmake" % templateThirdParty.GetBuildFolder()
 
 templateThirdParty.AddFilesToInstall( templateThirdParty.Glob("lib/nameofyourdlldebug.dll"), _debugOnly = 1, _WIN32 = 1)
 templateThirdParty.AddFilesToInstall( templateThirdParty.Glob("lib/nameofyourdllrelease.dll"), _releaseOnly = 1, _WIN32 = 1)
