2edef668d801b11b6e6bd70099d7688d95879f6b
ymartelli <ymartelli@941262cd-e152-0410-93b7-8f0684ea2129>
2010-08-25 03:26:49 +0000

    Integrated documentation in package.
    git-svn-id: https://svncistib.upf.edu/repos/toolkit/trunk/tools/StartNewModule@13677 941262cd-e152-0410-93b7-8f0684ea2129

--- src/setup.py
+++ src/setup.py
@@ -10,18 +10,15 @@ class exeSetup():
     ''' Helper class to generate windows executable.'''
     def __init__(self):
         pathToResources = "../resources"
-        pathToSrc = "../src/"
-        # Resource files to add to bin folder.
+        pathToSrc = "../src"
+        # Resources of the setup script
         self.resources = []
-        for root, dirs, files in os.walk(pathToResources):
-            if not root.find( ".svn" ) == -1: # skip svn hidden directories
-                continue
-            for name in files:
-                f1 = os.path.join(root, name)
-                f2 = "resources/" + root, [f1]
-                self.resources.append(f2)
+        # Resource files to add to bin folder.
+        self.__addToResources(pathToResources)
+        # Doc files to add to bin folder.
+        self.__addToResources("../doc/html")
         # script file
-        self.script = pathToSrc + "StartNewModule.py"
+        self.script = pathToSrc + "/StartNewModule.py"
         self.icon_resource = pathToResources + "/startnewmodule.ico"
         # options
         about = About()
@@ -45,6 +42,15 @@ class exeSetup():
                 }
             ],
             data_files = self.resources )
+
+    def __addToResources(self, origin):
+        for root, dirs, files in os.walk(origin):
+            if not root.find( ".svn" ) == -1: # skip svn hidden directories
+                continue
+            for name in files:
+                f1 = os.path.join(root, name)
+                f2 = "dist/" + root, [f1]
+                self.resources.append(f2)
 
 if __name__ == "__main__":
     mainSetup = exeSetup()
